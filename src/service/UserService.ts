8cd0df2e316fdf2c8dd10cd3a9e0fe8dc4d57a97
Thiti Dev <thiti.mwk.main@gmail.com>
2021-08-01 01:32:21 +0700

    UserService: injected its repository, created getUsers()

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,8 +1,20 @@
 import { Service, Inject } from "typedi";
+import { Repository } from "typeorm";
+import { InjectRepository } from "typeorm-typedi-extensions";
+import { User } from "../entity/User";
+import { UserRepository } from "../repository/UserRepository";
 
 @Service()
 export class UserService {
+  constructor(
+    @InjectRepository()
+    private readonly userRepository: UserRepository
+  ) {}
   async echo() {
     return "user echo";
+  }
+
+  async getUsers() {
+    return this.userRepository.getUsers();
   }
 }

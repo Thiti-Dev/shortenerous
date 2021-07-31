2151d1e12bf88924684c74605788b7bd2b0be656
Thiti Dev <thiti.mwk.main@gmail.com>
2021-08-01 01:32:50 +0700

    UserRepository: created the repository with the (fn)getUsers

--- src/repository/UserRepository.ts
+++ src/repository/UserRepository.ts
@@ -1,5 +1,11 @@
+import { Service } from "typedi";
 import { EntityRepository, Repository } from "typeorm";
 import { User } from "../entity/User";
 
+@Service()
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async getUsers() {
+    return await this.find();
+  }
+}

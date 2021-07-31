86dc29eebece904270b265b885d8d1026a897e8a
Thiti Dev <thiti.mwk.main@gmail.com>
2021-08-01 01:33:23 +0700

    UserEntity: decorated the fields with the type-graphql to define the gql-schema

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,30 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import { Field, Int, ObjectType } from "type-graphql";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
 
+@ObjectType()
 @Entity()
-export class User {
+export class User extends BaseEntity {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Field()
+  @Column()
+  firstName: string;
 
-    @Column()
-    firstName: string;
+  @Field()
+  @Column()
+  lastName: string;
 
-    @Column()
-    lastName: string;
+  @Field()
+  @Column()
+  email: string;
 
-    @Column()
-    age: number;
+  //@Field()
+  @Column()
+  password: string;
 
+  //@Field()
+  @Column()
+  salt: string;
 }

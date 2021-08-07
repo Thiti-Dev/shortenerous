3013555636d93d1fc1903ebe62d26057badc5d48
Thiti-Dev <thiti.mwk.main@gmail.com>
2021-08-08 01:41:33 +0700

    util->property: implemented generatic for (fn)applyPropertiesValueFrom

--- src/utils/common/property.ts
+++ src/utils/common/property.ts
@@ -1,9 +1,27 @@
 type Class<T = any> = new (...args: any[]) => T;
-function applyPropertiesValueFrom<T = Class>(base:Class,from:Object): T{
-  const properties = Object.getOwnPropertyNames(from)
-  const $class = new base()
-  for (let key of properties){
-    $class[key] = from[key]
-  }
-  return $class
+
+/**
+ * @description Applied all the property from target class to the base class
+ * @param base 
+ * @param from 
+ * @returns Class [that was passed in the T generic params]
+ * @example 
+ * ```ts
+ * const newlyAppliedClass = applyPropertiesValueFrom<AnyClass>(AnyClass,AnyClass2)
+ * 
+ * ```
+ */
+export function applyPropertiesValueFrom<T>(base:Class,from:Object):T{
+    if (!(base instanceof Object)) throw new Error('base arg is not a valid constructable class')
+    const properties = Object.getOwnPropertyNames(from)
+
+    try {
+        const $class = new (base as unknown as Class)() // catching this block
+        for (let key of properties){
+            $class[key] = from[key]
+        }
+        return $class
+    } catch (error) {
+        throw new Error('base arg is not a valid constructable class')
+    }
 }

4e97f445bddeaaf1f0c305af820bc27a2332b431
Thiti-Dev <thiti.mwk.main@gmail.com>
2021-08-09 01:15:48 +0700

    (Entity)User: created instance method -> validatePassword

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -7,6 +7,8 @@ import {
   Unique,
 } from "typeorm";
 
+import * as bcrypt from 'bcrypt';
+
 @ObjectType()
 @Entity()
 @Unique(["email"])
@@ -34,4 +36,9 @@ export class User extends BaseEntity {
   //@Field()
   @Column()
   salt: string;
+
+  async validatePassword(password: string): Promise<boolean> {
+    const hash = await bcrypt.hash(password, this.salt);
+    return hash === this.password;
+  }
 }
